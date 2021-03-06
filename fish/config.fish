c4ea08ed7d2521aae2add0a8feee65feef2231c7
Aethar01 <elliott.ashby88@gmail.com>
2021-03-06 18:21:23 +0000

    current essential dots

---
+++ fish/config.fish
@@ -0,0 +1,9 @@
+# Fish colors
+set -g fish_color_command --bold green
+set -g fish_color_error red
+set -g fish_color_quote yellow
+set -g fish_color_param white
+set -g fish_pager_color_selected_completion blue
+
+# Some config
+set -g fish_greeting
